c48c372fb5b5f5abcc7c2cf9846b14f1e49bd7d3
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-07-13 19:40:14 +0545

    keyboard and mouse

--- Main - SFML/ex5.cpp
+++ Main - SFML/ex5.cpp
@@ -1,34 +1,34 @@
-#include <SFML/Graphics.hpp>
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
-    //window.
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            /*if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }*/
-
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                window.close();
-            }
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            window.close();
-        }
-
-    }
-
-    return 0;
-}
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+//    //window.
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//            /*if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+//                window.close();
+//            }*/
+//
+//            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+//                window.close();
+//            }
+//        }
+//
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+//            window.close();
+//        }
+//        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+//            window.close();
+//        }
+//
+//    }
+//
+//    return 0;
+//}
